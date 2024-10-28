58925e7f2a672c520fb3079c0e286bb31fdf5007
Yichia <potatochipsss777@gmail.com>
2024-10-28 16:13:58 +0800

    新增footer

--- Client/pages/cart/index.js
+++ Client/pages/cart/index.js
@@ -1,11 +1,13 @@
-'use client'
-
 import React, { useState, useEffect } from 'react'
+import Footer from '@/components/home/common/footer'
+import FooterTwo from '@/components/home/common/footerTwo'
 
 export default function Index(props) {
   return (
     <>
       <div>購物車測試</div>
+      <FooterTwo />
+      <Footer />
     </>
   )
 }
