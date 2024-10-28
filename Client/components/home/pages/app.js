f5b1b68e4a6b4ecab74d7183d16a42482d1e1ec9
yichia <potatochipsss777@gmail.com>
2024-10-29 00:32:06 +0800

    解衝突

--- Client/components/home/pages/app.js
+++ Client/components/home/pages/app.js
@@ -3,11 +3,9 @@ import Header from '@/components/home/common/header'
 import ClassAct from '@/components/home/common/class-act'
 import Brands from '@/components/home/common/brands'
 import BestSller from '@/components/home/common/best-seller'
-<<<<<<< HEAD
 import Cardhome from '@/components/product/pages/card-home'
-=======
 import Footer from '@/components/home/common/footer'
->>>>>>> dev_chia
+
 export default function App(props) {
   return (
     <>
