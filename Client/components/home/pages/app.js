338c85731ffb06ff6ea7fa448bbd97690b5e6764
Peng <lynn7415963@gmail.com>
2024-10-28 13:55:57 +0800

    新增home>brands組件

--- Client/components/home/pages/app.js
+++ Client/components/home/pages/app.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Header from '@/components/home/common/header';
+import Brands from '@/components/home/common/brands';
 export default function App(props) {
   return (
     <>
       <Header />
+      <Brands/>
     </>
   );
 }
