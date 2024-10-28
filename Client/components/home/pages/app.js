b9ed39ee7a88a1762de062a3a2a52fa5e4bee9b8
Wen <gces3226@gmail.com>
2024-10-28 14:20:21 +0800

    新增home-class-act組件

--- Client/components/home/pages/app.js
+++ Client/components/home/pages/app.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Header from '@/components/home/common/header';
+import ClassAct from '@/components/home/common/class-act';
 export default function App(props) {
   return (
     <>
       <Header />
+      <ClassAct />
+
     </>
   );
 }
