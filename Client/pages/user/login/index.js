94b15ae203a933d462694bd45b99336804da11be
Wen <gces3226@gmail.com>
2024-10-28 15:44:23 +0800

    加入會員中心sb-header

--- Client/pages/user/login/index.js
+++ Client/pages/user/login/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import UserHeaderSb from '@/components/user/common/user-header-sb'
+import UserLogin from '@/components/user/pages/user-login'
 
 export default function Index(props) {
   return (
     <>
-      <UserHeaderSb />
+      <UserLogin />
     </>
   )
 }
