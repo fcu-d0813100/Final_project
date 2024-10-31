d37ef5fc62f682fc3e06b30d88403fec306b2be0
Wen <gces3226@gmail.com>
2024-10-31 22:40:57 +0800

    加入個人資訊

--- Client/pages/user/index.js
+++ Client/pages/user/index.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import UserSection from '@/components/user/common/user-section'
-
+import React, { useState, useEffect } from 'react';
+import InfoUser from '@/components/user/pages/info-user';
 export default function Index(props) {
   return (
     <>
-      <UserSection />
+      <InfoUser />
     </>
-  )
+  );
 }
