d003edc811483a5d10ffc020810774c57f6f3885
Wen <gces3226@gmail.com>
2024-11-01 10:19:43 +0800

    server bin 修改成3005

--- Server/bin/www.js
+++ Server/bin/www.js
@@ -15,7 +15,7 @@ import 'dotenv/config.js'
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '6005')
+var port = normalizePort(process.env.PORT || '3005')
 app.set('port', port)
 
 /**
