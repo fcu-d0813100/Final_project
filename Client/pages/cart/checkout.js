d3c876cb4f95c755f5d9c7f997367d27cbaaa77b
yichia <potatochipsss777@gmail.com>
2024-11-04 01:15:40 +0800

    套入課程資料

--- Client/pages/cart/checkout.js
+++ Client/pages/cart/checkout.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import Checkout from '@/components/cart/pages/checkout'
-import Header from '@/components/home/common/header'
-import FooterTwo from '@/components/home/common/footerTwo'
 
 export default function checkout() {
   return (
     <>
-      <Header />
       <Checkout />
-      <FooterTwo />
     </>
   )
 }
