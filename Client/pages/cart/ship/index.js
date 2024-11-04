937c818c157ab437c420436c0ac915ddb547cc93
Yichia <potatochipsss777@gmail.com>
2024-11-04 17:52:33 +0800

    訂單測試

--- Client/pages/cart/ship/index.js
+++ Client/pages/cart/ship/index.js
@@ -1,4 +1,5 @@
 import { useShip711StoreOpener } from '@/hooks/use-ship-711-store'
+import style from '@/pages/cart/ship/ship.module.scss'
 
 export default function Seven() {
   // useShip711StoreOpener的第一個傳入參數是"伺服器7-11運送商店用Callback路由網址"
@@ -13,17 +14,21 @@ export default function Seven() {
       <p>
         <br />
         <button
+          className={style.button}
           onClick={() => {
             openWindow()
           }}
         >
           選擇門市
         </button>
-        <br /> <br />
-        門市名稱: <input type="text" value={store711.storename} disabled />
-        <br />
-        <br />
-        門市地址: <input type="text" value={store711.storeaddress} disabled />
+        <div className={style.text}>
+          <span> 門市名稱 : </span>
+          <span className={style.title}>{store711.storename}</span>
+        </div>
+        <div>
+          <span> 門市地址 : </span>
+          <span className={style.title}>{store711.storeaddress}</span>
+        </div>
       </p>
       {/* <p>{JSON.stringify(store711)}</p> */}
     </>
