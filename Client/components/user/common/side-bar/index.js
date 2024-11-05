dc95d80b098a24242a567a015f1d58d44809b3cf
Wen <gces3226@gmail.com>
2024-11-06 01:34:13 +0800

    更新密碼

--- Client/components/user/common/side-bar/index.js
+++ Client/components/user/common/side-bar/index.js
@@ -17,49 +17,49 @@ import styles from './index.module.scss'
 
 const navLinks = [
   {
-    href: '/user', // 个人信息
+    href: '/user',
     icon: <PiUser size={65} />,
     label: '個人資訊',
     key: 'user',
   },
   {
-    href: '/user/password', // 更改密码
+    href: '/user/password',
     icon: <PiLockOpen size={65} />,
     label: '變更密碼',
     key: 'password',
   },
   {
-    href: '/user/order', // 订单查询
+    href: '/user/order',
     icon: <PiListMagnifyingGlass size={65} />,
     label: '訂單查詢',
     key: 'order',
   },
   {
-    href: '/user/favorite', // 我的收藏
+    href: '/user/favorite',
     icon: <PiHeartStraight size={65} />,
     label: '我的收藏',
     key: 'favorite',
   },
   {
-    href: '/user/workshop', // 我的课程
+    href: '/user/workshop',
     icon: <PiNotebook size={65} />,
     label: '我的課程',
     key: 'workshop',
   },
   {
-    href: '/user/post', // 我的贴文
+    href: '/user/post',
     icon: <PiListPlus size={65} />,
     label: '我的貼文',
     key: 'post',
   },
   {
-    href: '/user/activity', // 活动记录
+    href: '/user/activity',
     icon: <PiClockCountdown size={65} />,
     label: '活動紀錄',
     key: 'activity',
   },
   {
-    href: '/user/coupon', // 优惠券
+    href: '/user/coupon',
     icon: <PiTicket size={65} />,
     label: '優惠券',
     key: 'coupon',
@@ -68,15 +68,13 @@ const navLinks = [
 
 export default function Index() {
   const router = useRouter()
-  const { logout, auth } = useAuth() // 使用 useAuth 获取登录状态和 userId
+  const { logout } = useAuth()
   const [linkState, setLinkState] = useState(
     navLinks.reduce((acc, link) => {
       acc[link.key] = { hover: false, active: router.pathname === link.href }
       return acc
     }, {})
   )
-
-  const userId = auth.isAuth ? auth.userData.id : null // 从 useAuth 获取 userId
 
   useEffect(() => {
     setLinkState((prev) => {
@@ -113,32 +111,24 @@ export default function Index() {
         className={`${styles['collapse']} justify-content-center`}
       >
         <Nav>
-          {navLinks.map((link) => {
-            // 动态更新所有链接，基于 userId 动态拼接路径
-            const updatedHref =
-              userId && link.href.includes('/user/')
-                ? `${link.href}/${userId}` // 如果是 '/user' 开头的链接，动态加上 userId
-                : link.href // 否则保持原路径不变
-
-            return (
-              <Nav.Link
-                key={link.key}
-                href={updatedHref}
-                className={`${
-                  linkState[link.key].active
-                    ? styles['active']
-                    : linkState[link.key].hover
-                    ? styles['hover']
-                    : ''
-                } h6`}
-                onMouseEnter={() => handleMouseEnter(link.key)}
-                onMouseLeave={() => handleMouseLeave(link.key)}
-              >
-                {link.icon}
-                {link.label}
-              </Nav.Link>
-            )
-          })}
+          {navLinks.map((link) => (
+            <Nav.Link
+              key={link.key}
+              href={link.href}
+              className={`${
+                linkState[link.key].active
+                  ? styles['active']
+                  : linkState[link.key].hover
+                  ? styles['hover']
+                  : ''
+              } h6`}
+              onMouseEnter={() => handleMouseEnter(link.key)}
+              onMouseLeave={() => handleMouseLeave(link.key)}
+            >
+              {link.icon}
+              {link.label}
+            </Nav.Link>
+          ))}
           <button
             className="btn-logout h6"
             onClick={() => {
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import db from '##/configs/mysql.js'
 import multer from 'multer'
-
+// 檢查空物件, 轉換req.params為數字
+import { getIdParam } from '#db-helpers/db-tool.js'
 import jsonwebtoken from 'jsonwebtoken'
 // 中介軟體，存取隱私會員資料用
 import authenticate from '#middlewares/authenticate.js'
@@ -222,24 +223,76 @@ router.put(
   }
 )
 
-// // DELETE - 刪除會員資料
-// router.delete('/:id', async function (req, res) {
-//   const affectedRows = await User.destroy({
-//     where: {
-//       id,
-//     },
-//   })
+// =================================================================
+// post - 會員密碼更新
+// PUT - 更新會員資料(密碼更新用)
+router.put('/:id/password', authenticate, async function (req, res) {
+  const id = getIdParam(req)
 
-//   // 沒有刪除到任何資料 -> 失敗或沒有資料被刪除
-//   if (!affectedRows) {
-//     return res.json({
-//       status: 'fail',
-//       message: 'Unable to detele.',
-//     })
-//   }
+  // 檢查是否為授權會員，只有授權會員可以存取自己的資料
+  if (req.user.id !== id) {
+    return res.json({ status: 'error', message: '存取會員資料失敗' })
+  }
 
-//   // 成功
-//   return res.json({ status: 'success', data: null })
-// })
+  // user為來自前端的會員資料(準備要修改的資料)
+  const userPassword = req.body
+  // 檢查從前端瀏覽器來的資料，哪些為必要(name, ...)，從前端接收的資料為
+  // {
+  //   originPassword: '', // 原本密碼，要比對成功才能修改
+  //   newPassword: '', // 新密碼
+  // }
+  if (!id || !userPassword.origin || !userPassword.new) {
+    return res.json({ status: 'error', message: '缺少必要資料' })
+  }
+
+  // const [rows] = await db.query('SELECT * FROM user WHERE account = ?', [
+  //   loginUser.account,
+  // ])
+  // 查詢資料庫目前的資料
+  const [rows] = await db.query('SELECT password FROM user WHERE id = ?', [id])
+  const dbUser = rows[0]
+  // const dbUser = await User.findByPk(id, {
+  //   raw: true, // 只需要資料表中資料
+  // })
+
+  // null代表不存在
+  if (!dbUser) {
+    return res.json({ status: 'error', message: '使用者不存在' })
+  }
+
+  // compareHash(登入時的密碼純字串, 資料庫中的密碼hash) 比較密碼正確性
+  // isValid=true 代表正確
+  const isValid = await compareHash(userPassword.origin, dbUser.password)
+
+  // isValid=false 代表密碼錯誤
+  if (!isValid) {
+    return res.json({ status: 'error', message: '密碼錯誤' })
+  }
+
+  const hashedPassword = await generateHash(userPassword.new)
+
+  // 對資料庫執行update
+  const [affectedRows] = await db.query(
+    'UPDATE user SET password = ? WHERE id = ?',
+    [hashedPassword, id]
+  )
+  // const [affectedRows] = await User.update(
+  //   { password: userPassword.new },
+  //   {
+  //     where: {
+  //       id,
+  //     },
+  //     individualHooks: true, // 更新時要加密密碼字串 trigger the beforeUpdate hook
+  //   }
+  // )
+
+  // 沒有更新到任何資料 -> 失敗
+  if (!affectedRows) {
+    return res.json({ status: 'error', message: '更新失敗' })
+  }
+
+  // 成功，不帶資料
+  return res.json({ status: 'success', data: null })
+})
 
 export default router
