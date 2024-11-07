b8e0a919b5f17a416dfcf45d3e7ea36e9aead2fe
yuyunchen123 <joycemail88123@gmail.com>
2024-11-07 11:12:09 +0800

    撈完 information 資料

--- Client/components/teacher/pages/information/page-2.js
+++ Client/components/teacher/pages/information/page-2.js
@@ -1,4 +1,5 @@
 'use client'
+import { useAuth } from '@/hooks/use-auth'
 import Textarea from '@/components/teacher/common/t-dashboard-textarea-style'
 import InputStyle from '@/components/teacher/common/t-dashboard-input-style'
 import DashboardTitle from '@/components/shared/dashboard-title-y'
@@ -7,155 +8,194 @@ import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 
 export default function Page2({ onPreviousPage }) {
+  const role = 'admin'
+  const { auth, login, logout } = useAuth()
+  const { userData } = auth // 撈取 teacherData 資料
+  console.log(auth)
+
+  const [teacher, setTeacher] = useState(null)
+  const [gender, setGender] = useState('') // 新增 gender 狀態
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3005/api/teacher`)
+      if (!response.ok) {
+        throw new Error('網路回應不成功：' + response.status)
+      }
+      const data = await response.json()
+      const filteredData = data.find((teacher) => teacher.id === userData.id) // 篩選符合 userData.id 的資料
+      setTeacher(filteredData) // 只設定符合 id 的資料
+      console.log(filteredData)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleGenderChange = (e) => {
+    setGender(e.target.value)
+    setTeacher((prevTeacher) => ({ ...prevTeacher, gender: e.target.value })) // 更新 teacher.gender
+  }
+
   return (
     <>
       <div className={styles.main}>
         <DashboardTitle chTitle="個人資訊" enTitle="Information" />
-        <div
-          className={`${styles.basicInfoArea} d-flex justify-content-between align-items-center`}
-        >
-          <div className="col-8">
-            <div className={styles.account}>
-              <h4 className="m-0 h4">Gina Bettelli</h4>
-              <p className="m-0 ms-2">| account</p>
-            </div>
-
-            <div className={`${styles.inputArea} mt-5`}>
-              <div className="d-flex mb-3">
-                <InputStyle
-                  addclass="w-50"
-                  forText="name"
-                  titleCh="姓名"
-                  titleEn=" | name"
-                  typeText="text"
-                  placeholder="請輸入姓名"
-                  name=""
-                  value="Gina Bettelli"
-                />
-                <div className="mx-3"></div>
-                <InputStyle
-                  addclass="w-50"
-                  forText="email"
-                  titleCh="信箱"
-                  titleEn=" | email"
-                  typeText="text"
-                  placeholder="請輸入信箱"
-                  name=""
-                  value="ginabettelli@gmail.com"
-                />
-              </div>
-              <div className="d-flex justify-content-between align-items-start mb-3">
-                <InputStyle
-                  addclass="col-5"
-                  forText="nation"
-                  titleCh="國籍"
-                  titleEn=" | nation"
-                  typeText="text"
-                  placeholder="請輸入課程名稱"
-                  name=""
-                  value="美國"
-                />
-                <div className="mx-3 pb-3 ">
-                  <label
-                    htmlFor=""
-                    className={`${styles.radioLable} d-block p mb-3`}
-                  >
-                    性別
-                    <span className="ps"> | gender</span>
-                  </label>
-                  <div className="d-inline me-3">
-                    <input
-                      class="form-check-input p me-2 align-middle"
-                      type="radio"
-                      name="gender"
-                      id="inlineRadio1"
-                      value="male"
-                    />
-                    男
+        <form action="">
+          {teacher ? (
+            <div>
+              <div
+                className={`${styles.basicInfoArea} d-flex justify-content-between align-items-center`}
+              >
+                <div className="col-8">
+                  <div className={styles.account}>
+                    <h4 className="m-0 h4">{teacher.account}</h4>
+                    <p className="m-0 ms-2">| account</p>
                   </div>
-                  <div className="d-inline">
-                    <input
-                      class="form-check-input p me-2 align-middle"
-                      type="radio"
-                      name="gender"
-                      id="inlineRadio2"
-                      value="female"
-                      checked
+
+                  <div className={`${styles.inputArea} mt-5`}>
+                    <div className="d-flex mb-3">
+                      <InputStyle
+                        addclass="w-50"
+                        forText="name"
+                        titleCh="姓名"
+                        titleEn=" | name"
+                        typeText="text"
+                        placeholder="請輸入姓名"
+                        name=""
+                        value={teacher.name}
+                      />
+                      <div className="mx-3"></div>
+                      <InputStyle
+                        addclass="w-50"
+                        forText="email"
+                        titleCh="信箱"
+                        titleEn=" | email"
+                        typeText="email"
+                        placeholder="請輸入信箱"
+                        name=""
+                        value={teacher.email}
+                      />
+                    </div>
+                    <div className="d-flex justify-content-between align-items-start mb-3">
+                      <InputStyle
+                        addclass="col-5"
+                        forText="nation"
+                        titleCh="國籍"
+                        titleEn=" | nation"
+                        typeText="text"
+                        placeholder="請輸入課程名稱"
+                        name=""
+                        value={teacher.nation}
+                      />
+                      <div className="mx-3 pb-3 ">
+                        <label
+                          htmlFor="gender"
+                          className={`${styles.radioLable} d-block p mb-3`}
+                        >
+                          性別
+                          <span className="ps"> | gender</span>
+                        </label>
+                        <div className="d-inline me-3">
+                          <input
+                            class="form-check-input p me-2 align-middle"
+                            type="radio"
+                            name="gender"
+                            id="inlineRadio1"
+                            value="male"
+                            checked={teacher.gender === 'male'} // 當 teacher.gender 為 male 時選中
+                            onChange={handleGenderChange} // 加入 onChange 事件
+                          />
+                          男
+                        </div>
+                        <div className="d-inline">
+                          <input
+                            class="form-check-input p me-2 align-middle"
+                            type="radio"
+                            name="gender"
+                            id="inlineRadio2"
+                            value="female"
+                            checked={teacher.gender === 'female'} // 當 teacher.gender 為 female 時選中
+                            onChange={handleGenderChange} // 加入 onChange 事件
+                          />
+                          女
+                        </div>
+                      </div>
+                      <InputStyle
+                        addclass="col-4"
+                        forText="years of experience"
+                        titleCh="資歷"
+                        titleEn=" | years of experience"
+                        typeText="text"
+                        placeholder="請填入資歷/年"
+                        name=""
+                        value={teacher.years}
+                      />
+                    </div>
+                    <InputStyle
+                      addclass="col-6"
+                      forText="birthday"
+                      titleCh="生日"
+                      titleEn=" | birthday"
+                      typeText="date"
+                      placeholder="請選擇日期"
+                      name=""
+                      value={teacher.birthday}
                     />
-                    女
                   </div>
                 </div>
-                <InputStyle
-                  addclass="col-4"
-                  forText="years of experience"
-                  titleCh="資歷"
-                  titleEn=" | years of experience"
-                  typeText="text"
-                  placeholder="請填入資歷/年"
-                  name=""
-                  value="17"
-                />
+                <div className={`${styles.editImgArea}`}>
+                  <Image
+                    height={320}
+                    width={320}
+                    className={styles.editImg}
+                    src={`/teacher/teachers_img/T_${teacher.id}_color.jpg`}
+                    alt=""
+                  />
+                </div>
               </div>
-              <InputStyle
-                addclass="col-6"
-                forText="birthday"
-                titleCh="生日"
-                titleEn=" | birthday"
-                typeText="date"
-                placeholder="請選擇日期"
-                name=""
-                value="1983-07-12"
-              />
+
+              <hr className="opacity-75" />
+
+              <div className={styles.editMoreInfoArea}>
+                <div className={`${styles.editTextArea} d-flex gap-5`}>
+                  <Textarea
+                    title="關於我 About me"
+                    name="about"
+                    rows="10"
+                    width="100%"
+                    placeholder="最多輸入250字"
+                    addclass="w-100"
+                    value={teacher.about}
+                  />
+                  <Textarea
+                    title="我的 Slogan"
+                    name="notes"
+                    rows="10"
+                    width="100%"
+                    placeholder="最多輸入250字"
+                    addclass="w-100"
+                    value={teacher.slogan}
+                  />
+                  <Textarea
+                    title="經歷 Experience"
+                    name="experience"
+                    rows="10"
+                    width="100%"
+                    placeholder="最多輸入250字"
+                    addclass="w-100"
+                    value={teacher.experience}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className={`${styles.editImgArea}`}>
-            <Image
-              height={320}
-              width={320}
-              className={styles.editImg}
-              src="/teacher/teachers_img/T_1_color.jpg"
-            />
-          </div>
-        </div>
-
-        <hr className="opacity-75" />
-
-        <div className={styles.editMoreInfoArea}>
-          <div className={`${styles.editTextArea} d-flex gap-5`}>
-            <Textarea
-              title="關於我 About me"
-              name="about"
-              rows="10"
-              width="100%"
-              placeholder="最多輸入250字"
-              addclass="w-100"
-              value="現任職彩妝藝術總監。身為一個表演者，我喜歡後台的能量和創造力，
-              但無論在哪裡 - 在世界各地教授彩妝大師班，在全球舉行活動或參與密集的時裝週活動 - 品牌的多樣性仍然是我持續的靈感來源"
-            />
-            <Textarea
-              title="我的 Slogan"
-              name="notes"
-              rows="10"
-              width="100%"
-              placeholder="最多輸入250字"
-              addclass="w-100"
-              value="“我喜歡贈送口紅。 口紅如此豐富多變，一旦你用過了它們，你就離不開了！”"
-            />
-            <Textarea
-              title="經歷 Experience"
-              name="experience"
-              rows="10"
-              width="100%"
-              placeholder="最多輸入250字"
-              addclass="w-100"
-              value="於M·A·C Cosmetics 擔任資深彩妝師 11 年
-              在職業生涯中，曾與眾多全球頂級的時尚品牌如 Chanel、Dior、Gucci 和 Louis Vuitton 合作，
-              為他們的高端時裝秀創作出驚豔的妝容設計。她與眾多知名攝影師，如 Mario Testino、Annie Leibovitz 和 Steven Meisel 等合作，
-              參與了無數著名雜誌如《Vogue》、《Harper's Bazaar》 和《Elle》的封面拍攝。"
-            />
-          </div>
-        </div>
-
+          ) : (
+            <p>載入中或找不到資料</p>
+          )}
+        </form>
         <div className={`${styles.button} d-flex`}>
           <button
             className="btn-primary h6"
@@ -166,7 +206,9 @@ export default function Page2({ onPreviousPage }) {
           >
             回前頁
           </button>
-          <button className="btn-danger h6 ms-auto">儲存</button>
+          <button type="submit" className="btn-danger h6 ms-auto">
+            儲存
+          </button>
         </div>
       </div>
     </>
--- Server/routes/teacher.js
+++ Server/routes/teacher.js
@@ -6,18 +6,16 @@ const router = express.Router()
 router.get('/', async function (req, res) {
   const { search = '' } = req.query
   const sqlSelect = `SELECT
-  teachers.id,
-  teachers.name,
-  teachers.gender,
-  teachers.years,
-  teachers.nation,
-  teachers.valid,
+  teachers.*,
+  user.account AS user_account,
   workshop_type.type AS workshop_type_type
 
  FROM
     teachers
  JOIN
     workshop_type ON workshop_type.id = teachers.type_id 
+ LEFT JOIN
+    user ON user.id = teachers.id
  WHERE
     (teachers.name LIKE '%${search}%' OR teachers.nation LIKE '%${search}%' OR workshop_type.type LIKE '%${search}%')
     AND teachers.valid = 1  
@@ -30,44 +28,6 @@ router.get('/', async function (req, res) {
   res.json(result)
   console.log(req.params)
 })
-
-// router.get('/', async function (req, res, next) {
-//   const { search = '' } = req.query
-//   const sqlSelect = `SELECT
-//     workshop.id,
-//     workshop.name,
-//     workshop.price,
-//     workshop.type_id,
-//     teachers.id AS teacher_id,
-//     teachers.name AS teacher_name,
-//     GROUP_CONCAT(workshop_time.date ORDER BY workshop_time.date ASC) AS dates,
-//     workshop.registration_start,
-//     workshop.registration_end,
-//     workshop.isUpload,
-//     workshop.valid,
-//     workshop_type.type AS workshop_type_type
-//  FROM
-//     workshop
-//  JOIN
-//     teachers ON  workshop.teachers_id = teachers.id
-//  LEFT JOIN
-//       workshop_time ON workshop_time.workshop_id = workshop.id
-//  LEFT JOIN
-//     workshop_type ON workshop.type_id = workshop_type.id
-//  WHERE
-//      (workshop.name LIKE '%${search}%' OR teachers.name LIKE '%${search}%' OR workshop_type.type LIKE '%${search}%')
-//      AND workshop.valid = 1
-//      AND workshop.isUpload = 1
-//  GROUP BY
-//       workshop.id, teachers.id, workshop.isUpload, workshop.valid
-// `
-
-//   const result = await db
-//     .query(sqlSelect, [`%${search}%`, `%${search}%`, `%${search}%`])
-//     .catch((e) => console.log(e))
-//   res.json(result)
-//   //console.log(result)
-// })
 
 router.get('/:tid', async function (req, res) {
   const sqlSelect = `SELECT
