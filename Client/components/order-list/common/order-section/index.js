5fb1b046d410c435aa3caff47af7135dd6163fa1
Peng <lynn7415963@gmail.com>
2024-11-07 11:03:05 +0800

    刪除header footer

--- Client/components/order-list/common/order-section/index.js
+++ Client/components/order-list/common/order-section/index.js
@@ -3,13 +3,10 @@ import SideBar from '@/components/user/common/side-bar'
 import UserHeader from '@/components/user/common/user-header'
 import OrderTitle from '@/components/order-list/common/order-title'
 import styles from './index.module.scss'
-import Footer from '@/components/home/common/footer'
-import Header from '@/components/home/common/header'
 
 export default function index({ children, titleCN, titleENG }) {
   return (
     <>
-      <Header />
       <UserHeader />
       
       <div className={styles['user-section']}>
@@ -19,7 +16,7 @@ export default function index({ children, titleCN, titleENG }) {
           {children}
         </div>
       </div>
-      <Footer />
+
 
     </>
   )
