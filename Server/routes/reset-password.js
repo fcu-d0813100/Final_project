4618f0144c60ec6b090b068f9c3f4518af9af3f2
Wen <gces3226@gmail.com>
2024-11-09 21:32:40 +0800

    忘記密碼檔案修復

--- Server/routes/reset-password.js
+++ Server/routes/reset-password.js
@@ -1,3 +1,9 @@
+import express from 'express'
+const router = express.Router()
+import { createOtp, updatePassword } from '#db-helpers/otp.js'
+import transporter from '#configs/mail.js'
+import 'dotenv/config.js'
+
 const mailHtml = (otpToken) => `
 <html>
   <head>
@@ -81,3 +87,42 @@ const mailHtml = (otpToken) => `
   </body>
 </html>
 `
+
+// 使用時，將此HTML傳遞給您的電子郵件發送功能
+
+router.post('/otp', async (req, res, next) => {
+  const { email } = req.body
+  if (!email) return res.json({ message: 'fail', code: '400' })
+
+  const otp = await createOtp(email)
+  if (!otp.token) return res.json({ message: 'fail', code: '400' })
+
+  // 寄送email
+  const mailOptions = {
+    from: `"support"<${process.env.SMTP_TO_EMAIL}>`,
+    to: email,
+    subject: '重設密碼要求的電子郵件驗證碼',
+    html: mailHtml(otp.token),
+  }
+
+  transporter.sendMail(mailOptions, (err, response) => {
+    if (err) {
+      return res.status(400).json({ message: 'fail', detail: err })
+    } else {
+      return res.json({ message: 'email sent', code: '200' })
+    }
+  })
+})
+
+// 重設密碼用
+router.post('/reset', async (req, res, next) => {
+  const { email, token, password } = req.body
+
+  if (!token) return res.json({ message: 'fail', code: '400' })
+  const result = await updatePassword(email, token, password)
+
+  if (!result) return res.json({ message: 'fail', code: '400' })
+  return res.json({ message: 'success', code: '200' })
+})
+
+export default router
