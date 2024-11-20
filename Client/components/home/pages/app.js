47c921c00a6b8ca35245d9847bcba09c743b16f6
Peng <lynn7415963@gmail.com>
2024-11-20 16:05:31 +0800

    新增首頁season sale

--- Client/components/home/pages/app.js
+++ Client/components/home/pages/app.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import Header from '@/components/layout/common/header'
 import ClassAct from '@/components/home/common/class-act'
 import Brands from '@/components/home/common/brands'
 import BestSller from '@/components/home/common/best-seller'
 import Cardhome from '@/components/product/pages/card-home'
-import Footer from '@/components/layout/common/footer-global'
+import SeasonSale from '@/components/home/common/season-sale'
 
 export default function App(props) {
   return (
     <>
       <BestSller />
       <Cardhome />
+      <SeasonSale/>
       <ClassAct />
       <Brands />
     </>
